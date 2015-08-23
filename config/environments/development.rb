62bfac2499eb6131749a1549daaf8cfb0982ff80
Victor Borshchov <victor.borshchov@gmail.com>
2015-08-23 11:54:08 +0300

    Use Mandrill to sent confirmation emails from development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,8 +13,13 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
+  # change to true to allow email to be sent during development
+  config.action_mailer.perform_deliveries = true
+
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.default :charset => "utf-8"
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -40,4 +45,15 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
   
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.mandrillapp.com',
+    port:                 587,
+    enable_starttls_auto: true,
+    user_name:            ENV["GMAIL_USERNAME"],
+    password:             ENV["MANDRILL_APIKEY"],
+    authentication:       'login',
+    domain:               'tms-for-activebridge.herokuapp.com'
+  }
+  
 end
