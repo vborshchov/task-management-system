9bc7770dc4aadd489a2819307ff216fcd1290dde
Victor Borshchov <victor.borshchov@gmail.com>
2015-08-24 15:55:19 +0300

    fix double modal in batch delete

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,10 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Precompile additional assets (application.js, 
+  # application.css, and all non-JS/CSS are already added)
+  config.assets.precompile += %w( bootbox.js )
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
